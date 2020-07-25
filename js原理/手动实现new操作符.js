619e215c545363053925b23ff5e70716820668f2
Ly <952700304@qq.com>
2020-07-25 15:50:16 +0800

    feat: 更新内容

--- js原理/手动实现new操作符.js
+++ js原理/手动实现new操作符.js
@@ -1,15 +1,13 @@
 // 方法一，给__proto__赋值
-
 function newOne(funcName,...parm){
 	let res = {}
-	funcName.call(res,...parm)
 	// 等价于funcName.apply(res,parm)
 	res.__proto__ = funcName.prototype
+	funcName.call(res,...parm)
 	return res
 }
 
 // 方法二，调用Object.create方法
-
 function newOne_2(funcName,...parm){
 	let res = Object.create(funcName.prototype)//等价于给res.__proto__赋值funcName.prototype
 	funcName.call(res,...parm)
