8f3fc4d4ee06abd2592c5d7c0418045b029ebd67
ly15927086342 <952700304@qq.com>
2020-07-31 18:00:55 +0800

    feat: 更新原型继承和网络协议内容

--- js原理/原型继承.js
+++ js原理/原型继承.js
@@ -4,6 +4,12 @@
 // 或者 funcA.prototype.__proto__ = funcB.prototype
 // 或者 Object.setPrototypeOf( funcA.prototype, funcB.prototype );
 
+/*
+** js继承有两部分：
+** 属性继承（在构造函数内部调用另一个构造函数）
+** 方法继承（Object.create()）
+*/
+
 function Foo(name) {      
 	this.name = name; 
 } 
@@ -11,12 +17,14 @@ function Foo(name) {
 Foo.prototype.myName = function() {      
 	return this.name; 
 }; 
- 
+
+// 属性继承
 function Bar(name,label) {      
 	Foo.call( this, name ); //this取决于谁调用了Bar，由下文知new 调用，this指向new的实例 
 	this.label = label; 
 } 
- 
+
+// 方法继承
 // 我们创建了一个新的 Bar.prototype 对象并关联到 Foo.prototype 
 // 注意，这里不是给Bar创建一个原型，而是给Bar的原型创建一个原型（关联）
 // 因此 Bar.prototype没有myName方法，但是Bar.prototype.__proto__有myName的方法
@@ -24,6 +32,8 @@ Bar.prototype = Object.create( Foo.prototype );
 
 // 注意！现在没有 Bar.prototype.constructor 了 
 // 如果你需要这个属性的话可能需要手动修复一下它 
+
+Bar.prototype.constructor = Bar
  
 Bar.prototype.myLabel = function() {      
 	return this.label; 
